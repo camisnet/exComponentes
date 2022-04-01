ec42a511f6eb034782a152cc157fa22bc2857b94
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-04-01 18:43:47 -0300

    commit

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import Componente from './components/Componente';
+import Tabuada from './components/Tabuada';
 
 export default function App() {
+  const[num, setNum] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Componente nome="Maria" idade="40"/>
+
+      <View style={styles.row}>
+        <Text style={styles.texto}>Número</Text>
+        <TextInput
+          style={styles.input}
+          value={num}
+          onChangeText={(value)=>setNum(value)}
+        />
+      </View>
+      <Tabuada num={num}/>
     </View>
   );
 }
@@ -13,8 +25,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#acc6f1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  row:{
+    width:'100%'
+
+  },
+  input:{
+    width: '80%',
+    borderWidth: 1,
+    marginLeft: '10%',
+    fontSize: 30,
+    borderRadius:6
+  },
+  texto:{
+    fontSize:30,
+    marginLeft: '10%'
+  }
 });
